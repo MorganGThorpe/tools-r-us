d4aeeba1f09112a74dc1cc399db5e78c6a90e225
Matthew Gilligan <mcgilligan97@gmail.com>
2020-05-19 11:10:56 +0100

    adding to tool model

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -2,4 +2,9 @@ class Tool < ApplicationRecord
   belongs_to :user
 
   CATEGORIES = ["Garden", "Woodwork", "Building", "General Purpose", "Warehouse", "Motorised"]
+
+  vaidates :name, presence: true
+  vaidates :description, presence: true
+  vaidates :price, presence: true
+  vaidates :categories, inclusion: { in: CATEGORIES }
 end
