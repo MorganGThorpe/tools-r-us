c8158c02dacd46fe7b48722b79bf02412a312795
Helena Gagern <helena.gagern@gmail.com>
2020-05-20 11:17:53 +0100

    profile page and navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,7 +26,7 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
-import { functName } from './user';
+// import { functName } from './user';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
