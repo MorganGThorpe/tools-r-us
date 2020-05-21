68ca548dd849504d47e339ff5b696d2d95aa37a8
Matthew Gilligan <mcgilligan97@gmail.com>
2020-05-21 14:24:36 +0100

    creating reviews controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+  before_action :set_tool, only: [:new, :create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.tool = @tool
+    @review.save
+
+    redirect_to tool_path(@tool)
+  end
+
+  private
+
+  def set_tool
+    @tool = Tool.find(params[:booking_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
 
   resources :users, only: [:show]
+
   resources :tools do
     resources :bookings, only: [:create, :new]
-    resources : reviews, only: [:create, :index, :show]
+    resources :reviews, only: [:create, :index, :show]
   end
+
   resources :bookings, only: [:index, :show, :update, :destroy]
-
-
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
