1aaa32060a1cd75ef5f17dfb63fa75c8e357dad7
Matthew Gilligan <mcgilligan97@gmail.com>
2020-05-22 12:03:09 +0100

    adding user to review

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_05_21_155537) do
-=======
-ActiveRecord::Schema.define(version: 2020_05_21_125952) do
->>>>>>> baf17e3427964da8ba5ff35dd435f0e5eea064f5
+ActiveRecord::Schema.define(version: 2020_05_22_103226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +54,9 @@ ActiveRecord::Schema.define(version: 2020_05_21_125952) do
     t.bigint "tool_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["tool_id"], name: "index_reviews_on_tool_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "tools", force: :cascade do |t|
@@ -94,5 +92,6 @@ ActiveRecord::Schema.define(version: 2020_05_21_125952) do
   add_foreign_key "bookings", "tools"
   add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "tools"
+  add_foreign_key "reviews", "users"
   add_foreign_key "tools", "users"
 end
